c25a1f5c433c8a7b176f93826efd363e4a7ee0da
michaelziatkowski <michaelziatkowski@gmail.com>
2024-11-30 09:51:13 +0300

    Add a sample window with base elements in a single row.

--- app/root.py
+++ app/root.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from equipment import load_config
 
 items = load_config()
@@ -6,25 +7,34 @@ amps = []
 
 root = tk.Tk()
 root.title("Amplifiers' switcher")
-root.geometry("600x200")
+root.geometry("750x200")
 
-for item in items:
-    amps.append(item.name)
 
 selected_values = {}
 
-for num, amp in enumerate(amps, 1):
-    label = tk.Label(root, text=amp)
-    label.grid(row=num, column=0, padx=5, pady=5, sticky="w")
+number = ttk.Label(root, text="1")
+number.grid(row=1, column=0, padx=5, pady=5, sticky="w")
 
-    var = tk.StringVar(value="on")  # Here sets the default value
-    selected_values[amp] = var
+state_image = tk.PhotoImage(file="../img/green.png")
+state = ttk.Label(root, image=state_image)
+state.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 
-    on_button = tk.Radiobutton(root, text="on", variable=var, value="on")
-    on_button.grid(row=num, column=1, padx=5, pady=5, sticky="w")
+label = ttk.Label(root, text="AL1604D")
+label.grid(row=1, column=2, padx=5, pady=5, sticky="w")
 
-    off_button = tk.Radiobutton(root, text="off", variable=var, value="off")
-    off_button.grid(row=num, column=2, padx=5, pady=5, sticky="w")
+label = ttk.Label(root, text="Каминный зал")
+label.grid(row=1, column=3, padx=30, pady=5, sticky="w")
 
+on_button = ttk.Radiobutton(root, text="on", value="on")
+on_button.grid(row=1, column=4, padx=5, pady=5, sticky="w")
+
+off_button = ttk.Radiobutton(root, text="off", value="off")
+off_button.grid(row=1, column=5, padx=5, pady=5, sticky="w")
+
+ok_button = ttk.Button(root, text="ok")
+ok_button.grid(row=1, column=6, padx=40, pady=5, sticky="w")
+
+progress_bar = ttk.Progressbar(root, orient="horizontal", length="200")
+progress_bar.grid(row=1, column=7, padx=5, pady=5, sticky="w")
 
 root.mainloop()
