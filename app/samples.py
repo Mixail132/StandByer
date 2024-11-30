2545f838038e2c9f687a0969b2a1e3c84adb0efc
michaelziatkowski <michaelziatkowski@gmail.com>
2024-11-30 18:06:12 +0300

    Add sample requests as templates.

---
+++ app/samples.py
@@ -0,0 +1,55 @@
+set_standby_on_request_payload = {
+    "clientId": "x8-panel",
+    "payload": {
+        "type": "ACTION", "action": {
+            "type": "WRITE",
+            "values": [
+                {
+                    "id": "/Device/Audio/Presets/Live/Generals/Standby/Value",
+                    "data": {
+                        "type": "BOOL",
+                        "boolValue": True
+                    }
+                }
+            ]
+        }
+    }
+}
+
+set_standby_off_request_payload = {
+    "clientId": "x8-panel",
+    "payload": {
+        "type": "ACTION",
+        "action": {
+            "type": "WRITE",
+            "values": [
+                {
+                    "id": "/Device/Audio/Presets/Live/Generals/Standby/Value",
+                    "data": {
+                        "type": "BOOL",
+                        "boolValue": False
+                    }
+                }
+            ]
+        }
+    }
+}
+
+
+get_state_request_payload = {
+    "clientId": "x8-panel",
+    "payload": {
+        "type": "ACTION",
+        "action": {
+            "type": "READ",
+            "values": [
+                {
+                    "id": "/Device/Audio/Presets/Live/Generals/Standby/Value",
+                    "single": True
+                }
+            ]
+        }
+    }
+}
+
+
