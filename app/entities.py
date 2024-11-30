4a2cffe8cdecba17036efd0d5b9c4eaef18867f6
michaelziatkowski <michaelziatkowski@gmail.com>
2024-11-30 18:10:41 +0300

    Add an entity to hold the amplifier's configs.

--- app/entities.py
+++ app/entities.py
@@ -3,19 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class AmplifierConfig:
-    ip_address: str
+    ip: str
     zone: str
     type: str
     place: str
-
-
-@dataclass
-class AmplifierState:
-    standby: bool
-    online: bool
-
-
-@dataclass
-class RequestPayload:
-    check_state: str
-    set_state: str
+    state: int = -1
