3f5be814bfc725e78cba210f4a1dfb121fedb896
Mixail132 <millionbaxov@gmail.com>
2024-12-01 13:03:10 +0300

    Add a test command to read the radiobutton value and print it once the ok button has been clicked.

--- app/widgets.py
+++ app/widgets.py
@@ -1,14 +1,18 @@
 import tkinter as tk
 from tkinter import ttk
-from configs import read_config
 from actions import get_mock_state
 
-# amps = read_config()
 amps = get_mock_state()
 
 root = tk.Tk()
 root.title("Amplifiers' switcher")
 root.geometry("750x200")
+
+
+def set_standby(amp_ip):
+    selected_value = selected_values[amp_ip].get()
+    print(f"{amp.ip} set standby: {selected_value}")
+
 
 selected_values = {}
 
@@ -29,14 +33,14 @@ for num, amp in enumerate(amps, 1):
     zone_label.grid(row=num, column=3, padx=30, pady=5, sticky="w")
 
     var = tk.StringVar(value=amp.standby)
-    selected_values[amp.type] = var  # ???
+    selected_values[amp.ip] = var
     on_button = ttk.Radiobutton(root, text="on", value="on", variable=var)
     on_button.grid(row=num, column=4, padx=5, pady=5, sticky="w")
 
     off_button = ttk.Radiobutton(root, text="off", value="off", variable=var)
     off_button.grid(row=num, column=5, padx=5, pady=5, sticky="w")
 
-    ok_button = ttk.Button(root, text="ok")
+    ok_button = ttk.Button(root, text="ok", command=lambda ip=amp.ip: set_standby(ip))
     ok_button.grid(row=num, column=6, padx=40, pady=5, sticky="w")
 
     progress_bar = ttk.Progressbar(root, orient="horizontal", length="200")
