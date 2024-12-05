b52d46b13b15dd010a98d411bdf2a58f41bec647
Mixail132 <millionbaxov@gmail.com>
2024-12-05 18:49:22 +0300

    Update pop-up text when sending a command, update states in a function

--- app/widgets.py
+++ app/widgets.py
@@ -9,13 +9,11 @@ from configs import (read_config,
                      DeviceConfig)
 from tooltips import ToolTip, get_tooltip
 
-
 description: CommonTitles = read_description()
-dev_initials: list[DeviceConfig] = read_config()
-dev_states: list[DeviceConfig] = get_mock_states(dev_initials)
-devices: list[DeviceConfig] = get_tooltip(dev_states)
 
 root = tk.Tk()
+
+
 root.title(description.header)
 root.geometry("750x200")
 
@@ -23,6 +21,15 @@ progress_bars = {}
 selected_values = {}
 state_images = {}
 state_labels = {}
+tooltips = {}
+
+
+def update_states():
+    dev_initials: list[DeviceConfig] = read_config()
+    dev_states: list[DeviceConfig] = get_mock_states(dev_initials)
+    _devices: list[DeviceConfig] = get_tooltip(dev_states)
+    root.after(1000, update_states)
+    return _devices
 
 
 def get_command(unit: DeviceConfig) -> None:
@@ -57,7 +64,11 @@ def send_command(unit: DeviceConfig, selected_value: str) -> None:
         new_image = tk.PhotoImage(file=new_mark)
         state_labels[unit_ip].config(image=new_image)
         state_images[unit_ip] = new_image
+        get_tooltip([unit])
+        tooltips[unit_ip].text = unit.description
 
+
+devices = update_states()
 
 for num, device in enumerate(devices, 1):
 
@@ -73,7 +84,8 @@ for num, device in enumerate(devices, 1):
 
     type_label = ttk.Label(root, text=device.type)
     type_label.grid(row=num, column=2, padx=5, pady=5, sticky="w")
-    ToolTip(type_label, device.description)
+    tooltip = ToolTip(type_label, device.description)
+    tooltips[device.ip] = tooltip
 
     zone_label = ttk.Label(root, text=device.zone)
     zone_label.grid(row=num, column=3, padx=30, pady=5, sticky="w")
@@ -96,5 +108,7 @@ for num, device in enumerate(devices, 1):
     progress_bar = ttk.Progressbar(root, orient="horizontal", length="100")
     progress_bar.grid(row=num, column=7, padx=5, pady=5, sticky="w")
     progress_bars[device.ip] = progress_bar
+
+update_states()
 
 root.mainloop()
