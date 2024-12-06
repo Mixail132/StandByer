f14c475dc44bc8f2675bccbceb53e249b91cf9db
Mixail132 <millionbaxov@gmail.com>
2024-12-06 17:16:55 +0300

    Insert the current values to the setting window entry fields.

--- app/mainwins.py
+++ app/mainwins.py
@@ -24,7 +24,7 @@ state_labels = {}
 tooltips = {}
 
 
-def update_states():
+def update_states() -> list[DeviceConfig]:
     dev_initials: list[DeviceConfig] = read_config()
     dev_states: list[DeviceConfig] = get_mock_states(dev_initials)
     _devices: list[DeviceConfig] = get_tooltip(dev_states)
@@ -77,43 +77,43 @@ def change_state(unit):
 def main_window():
     devices = update_states()
 
-    for num, device in enumerate(devices, 1):
+    for number, device in enumerate(devices, 1):
 
-        number_label = ttk.Label(root, text=num)
-        number_label.grid(row=num, column=0, padx=5, pady=5, sticky="w")
+        number_label = ttk.Label(root, text=number)
+        number_label.grid(row=number, column=0, padx=5, pady=5, sticky="w")
 
         state_image = tk.PhotoImage(file=device.mark)
         state_label = ttk.Label(root, image=state_image)
         state_label.image = state_image
-        state_label.grid(row=num, column=1, padx=5, pady=5, sticky="w")
+        state_label.grid(row=number, column=1, padx=5, pady=5, sticky="w")
         state_labels[device.ip] = state_label
         state_images[device.ip] = state_image
 
         type_label = ttk.Label(root, text=device.type)
-        type_label.grid(row=num, column=2, padx=5, pady=5, sticky="w")
+        type_label.grid(row=number, column=2, padx=5, pady=5, sticky="w")
         tooltip = ToolTip(type_label, device.description)
         tooltips[device.ip] = tooltip
 
         zone_label = ttk.Label(root, text=device.zone)
-        zone_label.grid(row=num, column=3, padx=30, pady=5, sticky="w")
+        zone_label.grid(row=number, column=3, padx=30, pady=5, sticky="w")
 
         var = tk.StringVar(value=device.standby)
         selected_values[device.ip] = var
         on_button = ttk.Radiobutton(root, text="on", value="on", variable=var)
-        on_button.grid(row=num, column=4, padx=5, pady=5, sticky="w")
+        on_button.grid(row=number, column=4, padx=5, pady=5, sticky="w")
 
         off_button = ttk.Radiobutton(root, text="off", value="off", variable=var)
-        off_button.grid(row=num, column=5, padx=5, pady=5, sticky="w")
+        off_button.grid(row=number, column=5, padx=5, pady=5, sticky="w")
 
         ok_button = ttk.Button(
             root,
             text="ok",
             command=lambda unit=device: get_command(unit)
         )
-        ok_button.grid(row=num, column=6, padx=40, pady=5, sticky="w")
+        ok_button.grid(row=number, column=6, padx=40, pady=5, sticky="w")
 
         progress_bar = ttk.Progressbar(root, orient="horizontal", length="106")
-        progress_bar.grid(row=num, column=7, padx=5, pady=5, sticky="w")
+        progress_bar.grid(row=number, column=7, padx=5, pady=5, sticky="w")
         progress_bars[device.ip] = progress_bar
 
     update_states()
@@ -121,7 +121,8 @@ def main_window():
     settings_button = ttk.Button(
         root,
         text="Settings",
-        command=lambda main=root: settings_window(root))
+        command=lambda main=root: settings_window(main)
+    )
     settings_button.grid(row=6, column=8, padx=35, pady=25, sticky="w")
 
     root.mainloop()
--- app/setwins.py
+++ app/setwins.py
@@ -1,10 +1,13 @@
+import ipaddress
 import tkinter as tk
 from tkinter import ttk
-from entities import CommonTitles
+from entities import CommonTitles, DeviceConfig
 from configs import read_description
+from configs import read_config
 
+
+devices: list[DeviceConfig] = read_config()
 description: CommonTitles = read_description()
-
 
 def settings_window(root):
 
@@ -14,36 +17,56 @@ def settings_window(root):
 
     name_label = ttk.Label(settings, text=description.name)
     name_label.grid(row=1, column=1, padx=5, pady=10, sticky="w")
+
     type_label = ttk.Label(settings, text=description.type)
     type_label.grid(row=1, column=2, padx=5, pady=10, sticky="w")
+
     zone_label = ttk.Label(settings, text=description.zone)
     zone_label.grid(row=1, column=3, padx=5, pady=10, sticky="w")
+
     ip_label = ttk.Label(settings, text=description.ip)
     ip_label.grid(row=1, column=4, padx=5, pady=10, sticky="w")
+
     place_label = ttk.Label(settings, text=description.place)
     place_label.grid(row=1, column=5, padx=5, pady=10, sticky="w")
 
-    for i in range(2, 7, 1):
+    for number, device in enumerate(devices, 2):
 
-        number_label = ttk.Label(settings, text=i-1)
-        number_label.grid(row=i, column=0, padx=5, pady=10, sticky="w")
+        number_label = ttk.Label(settings, text=number-1)
+        number_label.grid(row=number, column=0, padx=5, pady=10, sticky="w")
 
-        name_label = ttk.Entry(settings)
-        name_label.grid(row=i, column=1, padx=5, pady=10, sticky="w")
+        name_label = ttk.Entry(settings, width=28)
+        name_label.insert(0, device.name)
+        name_label.grid(row=number, column=1, padx=5, pady=10, sticky="w")
 
-        name_label = ttk.Entry(settings)
-        name_label.grid(row=i, column=2, padx=5, pady=10, sticky="w")
+        type_label = ttk.Entry(settings, width=15)
+        type_label.insert(0, device.type)
+        type_label.grid(row=number, column=2, padx=5, pady=10, sticky="w")
 
-        name_label = ttk.Entry(settings)
-        name_label.grid(row=i, column=3, padx=5, pady=10, sticky="w")
+        zone_label = ttk.Entry(settings, width=20)
+        zone_label.insert(0, device.zone)
+        zone_label.grid(row=number, column=3, padx=5, pady=10, sticky="w")
 
-        name_label = ttk.Entry(settings)
-        name_label.grid(row=i, column=4, padx=5, pady=10, sticky="w")
+        ip_label = ttk.Entry(settings, width=15)
+        ip_label.insert(0, device.ip)
+        ip_label.grid(row=number, column=4, padx=5, pady=10, sticky="w")
 
-        name_label = ttk.Entry(settings)
-        name_label.grid(row=i, column=5, padx=5, pady=10, sticky="w")
+        place_label = ttk.Entry(settings, width=12)
+        place_label.insert(0, device.place)
+        place_label.grid(row=number, column=5, padx=5, pady=10, sticky="w")
 
     apply_button = ttk.Button(settings, text="Apply")
     apply_button.grid(row=7, column=6, padx=10, pady=25, sticky="w")
 
     settings.mainloop()
+
+
+def validate_ip(host):
+
+    try:
+        is_valid = ipaddress.ip_address(host)
+        ip_is_valid = bool(is_valid)
+    except ValueError:
+        ip_is_valid = False
+
+    return ip_is_valid
