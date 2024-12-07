dd99b001296cd294b88b70d0f122aeb63df92d3a
Mixail132 <millionbaxov@gmail.com>
2024-12-07 21:24:16 +0300

    Fix the lost settings window table header.

--- app/settings_window.py
+++ app/settings_window.py
@@ -28,19 +28,19 @@ def settings_window(root):
     settings.geometry("680x320")
 
     name_label = ttk.Label(settings, text=description.name)
-    name_label.grid(row=1, column=1, padx=5, pady=10, sticky="w")
+    name_label.grid(row=0, column=1, padx=5, pady=5, sticky="w")
 
     type_label = ttk.Label(settings, text=description.type)
-    type_label.grid(row=1, column=2, padx=5, pady=10, sticky="w")
+    type_label.grid(row=0, column=2, padx=5, pady=5, sticky="w")
 
     zone_label = ttk.Label(settings, text=description.zone)
-    zone_label.grid(row=1, column=3, padx=5, pady=10, sticky="w")
+    zone_label.grid(row=0, column=3, padx=5, pady=5, sticky="w")
 
     ip_label = ttk.Label(settings, text=description.ip)
-    ip_label.grid(row=1, column=4, padx=5, pady=10, sticky="w")
+    ip_label.grid(row=0, column=4, padx=5, pady=5, sticky="w")
 
     place_label = ttk.Label(settings, text=description.place)
-    place_label.grid(row=1, column=5, padx=5, pady=10, sticky="w")
+    place_label.grid(row=0, column=5, padx=5, pady=5, sticky="w")
 
     for device in devices:
 
