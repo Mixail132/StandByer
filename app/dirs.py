8007cad139c08633475b6ff09994a0a145a3f792
Mixail132 <millionbaxov@gmail.com>
2024-12-11 18:56:15 +0300

    Add relative paths holding file.

---
+++ app/dirs.py
@@ -0,0 +1,8 @@
+""" Making the relative paths to the project files."""
+
+from pathlib import Path
+
+DIR_ROOT = Path(__file__).parent.parent.resolve()
+DIR_APP = DIR_ROOT / "app"
+DIR_TEMP = DIR_ROOT / ".temp"
+DIR_IMG = DIR_ROOT / "img"
