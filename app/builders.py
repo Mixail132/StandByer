4fc4b78a70ceb352c07f5a6dfd3877c77c8b7ee7
Mixail132 <millionbaxov@gmail.com>
2024-12-12 15:10:34 +0300

    Refactor the code, copy the files needed to the 'out' folder place the output file to the 'out' folder when making the 'exe' file.

--- app/builders.py
+++ app/builders.py
@@ -3,36 +3,37 @@
 import subprocess
 from pathlib import Path
 
-from app.dirs import DIR_APP, DIR_IMG, DIR_TEMP
+from app.dirs import DIR_APP, DIR_IMG, DIR_TEMP, DIR_OUT
 
 
-def glue_scripts(
+def glue_scripts_needed(
     output_script: Path,
     input_scripts: list[Path],
 ) -> None:
-    """Combines all the scripts to a single one."""
+    """Combine all the scripts to a single result one."""
     with open(output_script, "w", encoding="utf-8") as out:
+
         for script in input_scripts:
+
             with open(script, "r", encoding="utf-8") as lines:
+
                 for line in lines:
+
                     if "app." in line:
                         continue
-                    if "FILE_VARS.is_file():" in line:
-                        continue
-                    if '"example_vars.ini"' in line:
-                        continue
-                    if "FILE_VARS = DIR_APP / " in line:
-                        line = line.replace("DIR_APP / ", "")
-                    if "__name__" in line:
-                        break
+
+                    if "device.mark = DIR_IMG / " in line:
+                        line = line.replace("DIR_IMG / ", "")
+
                     out.write(line)
+
                 out.write("\n\n")
 
 
-def format_scripts(
+def format_result_script(
     output_script: Path,
 ) -> None:
-    """Formats the total script using linters."""
+    """Format the result script using linters."""
     subprocess.run(
         [
             "autopep8",
@@ -46,8 +47,34 @@ def format_scripts(
     subprocess.run(["isort", output_script], check=True)
 
 
+def copy_files_needed() -> None:
+    """Create an output folder and puts the necessary files into it."""
+
+    if not DIR_OUT.exists():
+        DIR_OUT.mkdir(exist_ok=True, mode=0o777)
+
+    copied_files = [
+        DIR_APP / ".env",
+        DIR_IMG / "green.png",
+        DIR_IMG / "grey.png",
+        DIR_IMG / "red.png",
+    ]
+    for file in copied_files:
+        out_file = DIR_OUT / file.name
+        if not out_file.exists():
+            subprocess.run(
+                [
+                    "copy",
+                    file,
+                    DIR_OUT,
+                ],
+                check=True,
+                shell=True
+            )
+
+
 def build_exe_file(output_script: Path) -> None:
-    """Builds an exe file from the total script."""
+    """Build an exe file from the result script."""
     subprocess.run(
         [
             "pyinstaller",
@@ -60,7 +87,7 @@ def build_exe_file(output_script: Path) -> None:
             "switcher",
             f"{output_script}",
             "--distpath",
-            DIR_APP,
+            DIR_OUT,
             "--workpath",
             DIR_TEMP / "build",
             "--specpath",
@@ -85,6 +112,7 @@ if __name__ == "__main__":
 
     output_file = DIR_APP / "commons.py"
 
-    glue_scripts(output_file, files)
-    format_scripts(output_file)
+    glue_scripts_needed(output_file, files)
+    format_result_script(output_file)
+    copy_files_needed()
     build_exe_file(output_file)
