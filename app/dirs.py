2bcf2c1f694f95dd0c388f88c611eb3e1ac3fbd8
Mixail132 <millionbaxov@gmail.com>
2024-12-22 09:05:27 +0300

    Add the linters settings directory path.

--- app/dirs.py
+++ app/dirs.py
@@ -3,7 +3,9 @@
 from pathlib import Path
 
 DIR_ROOT = Path(__file__).parent.parent.resolve()
+
 DIR_APP = DIR_ROOT / "app"
-DIR_TEMP = DIR_ROOT / ".temp"
 DIR_IMG = DIR_ROOT / "img"
+DIR_LINT = DIR_ROOT / ".github" / "settings"
 DIR_OUT = DIR_ROOT / "out"
+DIR_TEMP = DIR_ROOT / ".temp"
